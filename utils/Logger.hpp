341c995fcacbccf3621be085160d8555c5f6816d
justinvjoseph <justinvjoseph@gmail.com>
2023-10-23 20:10:02 -0700

    logger v0

---
+++ utils/Logger.hpp
@@ -0,0 +1,55 @@
+
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <fstream>
+
+namespace rms_utils {
+
+enum class LogLevel {
+	NONE = 0,
+	ERROR = 1,
+	WARN = 2,
+	INFO = 3,
+	DEBUG = 4
+};
+
+enum class LogOutput {
+	CONSOLE,
+	FILE
+};
+
+/**
+* Logger Class Used to Output Details of Current Application Flow
+*/
+class Logger {
+public:
+	static std::shared_ptr<Logger> GetInstance();
+
+	void SetLogPreferences(std::string logFileName,
+						   LogLevel level,
+						   LogOutput output);
+
+	void Log(std::string codeFile, int codeLine, std::string message, LogLevel messageLevel);
+
+	LogOutput GetLogOutput(const std::string& logOutput);
+	LogLevel GetLogLevel(const std::string& logLevel);
+
+private:
+	LogLevel logLevel;
+	LogOutput logOutput;
+	std::ofstream logFile;
+
+	static std::shared_ptr<Logger> loggerInstance;
+
+	void LogMessage(const std::string& message);
+
+};
+
+} //namespace rms_utils
+
+#endif
